70a6f6ffa5fc8baaec53b8c9a3d6c94895c4d288
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-10-20 15:21:51 +0530

    Add files via upload

---
+++ literals.dart
@@ -0,0 +1,15 @@
+void main(){
+  String s1='single';
+  String s2="double";
+  String s3='it\'s easy';
+  String s4="it's easy";
+  String s5='this is the my first time coding in the dart language.'
+             +'this is the easy way to creating app';
+
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+print(s5);
+}
